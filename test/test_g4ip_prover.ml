c10aa38d7b1e81accd20e260baf8e481db8329fb
Frank <fsacco@andrew.cmu.edu>
2023-12-20 23:45:52 -0500

    More extensive testing

--- test/test_g4ip_prover.ml
+++ test/test_g4ip_prover.ml
@@ -12,7 +12,13 @@ let tests = "tests" >::: [
   (*more complex tests*)
   "AAndAAImpliesBImpliesB" >:: (fun _ -> assert_equal (G4ip.prove (Implies(And(Atom("A"), Implies(And(Atom("A"), Atom("A")), Atom("B"))), Atom("B")))) true);
   "AAndAImpliesBImpliesBAndB" >:: (fun _ -> assert_equal (G4ip.prove (Implies(And(Atom("A"), Implies(Atom("A"), Atom("B"))), And(Atom("B"), Atom("B"))))) true);
+  "DoubleNegLEM" >:: (fun _ -> assert_equal (G4ip.prove (Implies(Implies(Or(Atom("A"), Implies(Atom("A"), Atom("F"))), Atom("F")), Atom("F")))) true);
   "true1" >:: (fun _ -> assert_equal (G4ip.prove (Implies(And(Atom("A"), Implies(Implies(Atom("A"), Atom("B")), Atom("C"))), And(Atom("A"), Implies(Atom("B"), Atom("C")))))) true);
+  "true2" >:: (fun _ -> assert_equal (G4ip.prove (Implies(Implies(Atom("B"), And(Atom("C"), Atom("B"))), Implies(Atom("B"), And(Atom("C"), Atom("B")))))) true);
+  "true3" >:: (fun _ -> assert_equal (G4ip.prove (Implies(And(Atom("A"), Atom("B")), And(Atom("A"), Atom("B"))))) true);
+  "true4" >:: (fun _ -> assert_equal (G4ip.prove (Implies(Implies(Or(Atom("B"), Atom("A")), Atom("F")), Implies(Atom("B"), Atom("F"))))) true);
+  "true5" >:: (fun _ -> assert_equal (G4ip.prove (Implies(And(Atom("A"), Implies(Atom("B"), Atom("C"))), And(Atom("A"), Implies(Implies(Atom("A"), Atom("B")), Atom("C")))))) true);
+  "true6" >:: (fun _ -> assert_equal (G4ip.prove (Implies(Implies(Atom("A"), Atom("B")), Implies(Implies(Implies(Atom("A"), Atom("C")), Atom("C")), Implies(Implies(Atom("A"), Atom("C")), Atom("C")))))) true);
 
   (*false tests*)
   "A" >:: (fun _ -> assert_equal (G4ip.prove (Atom("A"))) false);
